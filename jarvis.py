e6fae5c9905541dc8e2c44a42e5f5e7c00a94438
Dennis Kamunya <kinc.developer@gmail.com>
2021-02-23 20:44:26 +0300

    Create speak fn

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,10 @@
 import pyttsx3 # pip install pyttsx3
 
 engine = pyttsx3.init() # Initialize pyttsx3
-engine.say('Hello world')
-engine.runAndWait()
+
+def speak(audio):
+	engine.say(audio)
+	engine.runAndWait()
+
+speak('Hello world')
 
