3193198844d0340c47676aeb56905663648c5a7d
Dennis Kamunya <kinc.developer@gmail.com>
2021-02-23 20:50:15 +0300

    Create time fn

--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,5 @@
 import pyttsx3 # pip install pyttsx3
+from datetime import datetime as dt
 
 engine = pyttsx3.init() # Initialize pyttsx3
 
@@ -6,5 +7,11 @@ def speak(audio):
 	engine.say(audio)
 	engine.runAndWait()
 
-speak('Hello world')
+def time_():
+	time_=dt.now().strftime("%I:%M:%S") # 12 hour clock
+	speak("The  current time is ")
+	speak(time_)
+
+time_()
+
 
